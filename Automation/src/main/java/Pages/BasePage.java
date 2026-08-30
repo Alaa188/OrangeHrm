6560df35227aa079f7fd5ad6aede3d406cee356b
Alaa188 <alaasaeed731@gmail.com>
2026-08-30 20:06:22 +0300

    Update Java version to 21

---
+++ Automation/src/main/java/Pages/BasePage.java
@@ -0,0 +1,11 @@
+package Pages;
+
+public class PageBase {
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+}
